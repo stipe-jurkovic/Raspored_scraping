94a2f47b67aa71dcd3519ad00802ed4e66901980
stipe-jurkovic <sjurko00@fesb.hr>
2025-03-18 15:38:26 +0100

    added saving free and occupied times to file, formatted some strings

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,24 +2,44 @@ import okhttp3.OkHttpClient
 import java.io.File
 import java.time.DayOfWeek
 import java.time.LocalDate
-import java.time.LocalDateTime
 import java.time.LocalTime
+import java.time.format.DateTimeFormatter
 
 suspend fun main() {
     val userNames = readUsernamesFromFile("data/usernames.txt")
     if (userNames.isEmpty()) {
-        throw IllegalArgumentException("usernames.txt should contain at least 1 username")
+        println("usernames.txt should contain at least 1 username")
+        return
     }
+
     val dates = readUsernamesFromFile("data/dates.txt")
     if (dates.size != 2) {
-        throw IllegalArgumentException("dates.txt should contain exactly 2 dates")
+        println("dates.txt should contain exactly 2 dates")
+        return
     }
-    val startDate = dates.first()
-    val endDate = dates[1]
+    val startDate = LocalDate.parse(dates[0], DateTimeFormatter.ofPattern("dd-MM-yyyy"))
+    val endDate = LocalDate.parse(dates[1], DateTimeFormatter.ofPattern("dd-MM-yyyy"))
 
-    print("Fetching timetables for ${userNames.size} users from $startDate to $endDate\n")
+    if (startDate >= endDate) {
+        println("Start date must be before end date")
+        return
+    }
+
+    val times = readUsernamesFromFile("data/times.txt")
+    if (dates.size != 2) {
+        println("times.txt should contain exactly 2 times")
+        return
+    }
+    val startTime = LocalTime.parse(times[0], DateTimeFormatter.ofPattern("HH:mm"))
+    val endTime = LocalTime.parse(times[1], DateTimeFormatter.ofPattern("HH:mm"))
+    if (startTime >= endTime) {
+        println("Start time must be before end time")
+        return
+    }
+
+    print("Fetching timetables for ${userNames.size} users from ${startDate.toStringCustom()} to ${endDate.toStringCustom()}\n")
     println("For each user, a CSV file will be created in the data/timetables directory.")
-    println("The bigger time range, the longer it will take to fetch the data for each user.")
+    println("The bigger time range, the longer it will take to fetch the data for each user.\n")
 
     val timetableService = TimetableService(OkHttpClient())
     val timeTableRepository = TimeTableRepository(timetableService)
@@ -27,8 +47,8 @@ suspend fun main() {
     val allEvents = userNames.map { userName ->
         try {
             val events = timeTableRepository.fetchTimetable(userName, startDate, endDate)
-            saveToCsv(userName, events, startDate, endDate)
-            print("$userName done\n")
+            saveToCsv(userName, events, startDate.toStringCustom(), endDate.toStringCustom())
+            print("Fetching $userName's timetable completed\n")
             events
         } catch (e: Exception) {
             print("Error fetching timetable for $userName: ${e.message}\n")
@@ -37,12 +57,14 @@ suspend fun main() {
     }
 
     val periods = mergePeriods(allEvents)
-    val freeTimes = flipPeriods(periods, LocalTime.of(8, 0), LocalTime.of(20, 0))
-
-    print("done")
+    saveTimesToCsv(filename = "occupiedTimes", periods)
+    val freeTimes = flipPeriods(periods, startTime, endTime)
+    saveTimesToCsv(filename = "freeTimes", freeTimes)
+    println("Free times saved to data/freeTimes.csv")
 }
 
 fun mergePeriods(allEvents: List<List<Event>>): Map<DayOfWeek, MutableList<PeriodTime>> {
+
     return allEvents.flatten().groupBy { it.start.dayOfWeek }.toSortedMap().map { (day, per) ->
         val events = per.map { event -> PeriodTime(event.start.toLocalTime(), event.end.toLocalTime()) }.distinct()
             .sortedBy { it.end }.sortedBy { it.start }
@@ -86,7 +108,11 @@ fun flipPeriods(
 }
 
 fun readUsernamesFromFile(filename: String): List<String> {
-    return File(filename).readLines().filter { it.isNotBlank() }
+    return File(filename).readLines().filter { it.isNotBlank() }.filter { !it.startsWith("#") }
+}
+
+fun LocalDate.toStringCustom(): String {
+    return this.format(DateTimeFormatter.ofPattern("dd-MM-yyyy"))
 }
 
 fun saveToCsv(userName: String, events: List<Event>, startDate: String, endDate: String) {
@@ -98,6 +124,21 @@ fun saveToCsv(userName: String, events: List<Event>, startDate: String, endDate:
         out.println(events.first().toCSVHeader())
         events.forEach { event ->
             out.print(event.toCSV() + "\r\n")
+        }
+    }
+}
+
+fun saveTimesToCsv(filename: String, freeTimes: Map<DayOfWeek, MutableList<PeriodTime>>) {
+    if (File("data/freeTimes").mkdirs()) {
+        print("Directory data created\n")
+    }
+    val file = File("data/freeTimes/$filename.csv")
+    file.printWriter().use { out ->
+        out.println("DayOfWeek,StartTime,EndTime")
+        freeTimes.forEach { (day, periods) ->
+            periods.forEach { period ->
+                out.print("$day,${period.start},${period.end}\r\n")
+            }
         }
     }
 }
